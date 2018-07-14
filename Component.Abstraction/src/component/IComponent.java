98239aa031edd8ee0e2130d8277d6167eb2f1cf1
patzke <marianpatzke@gmail.com>
2018-07-14 19:19:50 +0200

    Projektstrukur verändert

---
+++ Component.Abstraction/src/component/IComponent.java
@@ -0,0 +1,16 @@
+package component;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author marian
+ */
+public interface IComponent<TState, TResult> {
+    String GetID();
+    TResult Excute(TState state) throws Exception; 
+}
---
+++ ComponentPool.Abstraction/src/services/IComponentProvider.java
@@ -0,0 +1,23 @@
+package services;
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import component.IComponent;
+
+/**
+ *
+ * @author marian
+ */
+public interface IComponentProvider {
+    <TState, TResult> IComponent<TState, TResult> Get(String id) throws Exception;
+    void ReloadAll();
+    void Reload(String id) throws Exception;
+}
+ 
